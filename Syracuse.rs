3ee35ec0dfea63dfe80633f409e6c0f4ed723a61
Youness <youness.kafia@devinci.fr>
2019-09-10 11:36:16 +0200

    clear console

--- Syracuse.rs
+++ Syracuse.rs
@@ -1,15 +1,25 @@
 mod base_types;
 use base_types::Value;
 
+fn clear() {
+    if std::process::Command::new("cls").status().unwrap().success() {
+        println!("screen successfully cleared");
+    }
+}
+
+
 fn main() {
 
     
-    let mut a = Value {x:0,y:0};
+    let  a = Value {x:0,y:0};
     println!("{}",a);
-    while true
-    {
-        a+=1;
-        println!("{}",a);
-    }
+    clear();
+    // while a.y<1
+    // {
+    //     a+=1;
+    //     println!("{}",a);
+
+    // }
     
 }
+
